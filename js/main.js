d38705fd666a4634ec844e1f12338ccbefa58fec
Sebastian Fortuna <sebastian.fortuna@farmacity.com.ar>
2020-11-04 20:35:14 -0300

    commit clase 2

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let datos = [
+    {
+        id: 1,
+        titulo: 'Garbanzo'
+    },
+    {
+        id: 2,
+        titulo: 'Jabon'
+    }
+];
+
+const btnAgregar = document.querySelector('#btnAgregar');
+const inpBusqueda = document.querySelector('#inpBusqueda');
+const btnDeshacer = document.querySelector('#btnDeshacer');
+
+// Funciones
+
+
+
+
+
+//Eventos
+
+
+
+
+
+
+//Variables
